ab040c5a10160e0c6500a026ccba170be8b1fafe
Stephen Punwasi <stephen.punwasi@gmail.com>
2014-10-25 21:01:22 -0400

    Added WP Head Inject Wrapper

--- wp-module.php
+++ wp-module.php
@@ -6,4 +6,12 @@ Version: 0.0.1
 Author: Stephen Punwasi
 Description: Quickstart template for Wordpress Plugins
 */
+
+//ADD ACTION AFTER CALLING THE WP HEAD
+add_action( 'wp_head', 'sp_inject_page_header_output' );
+
+function sp_inject_page_header_output() { ?>
+//INSERT SCRIPT TO ENTER HERE
+ <?php }
+
 ?>
