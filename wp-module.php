bbd50e6b0f778a74eed7d5067632957af9a9ace7
Stephen Punwasi <stephen.punwasi@gmail.com>
2014-10-25 20:49:45 -0400

    Header Added

---
+++ wp-module.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Wordpress Plugin Boilerplate
+Plugin URI: http://www.stephenpunwasi.com/
+Version: 0.0.1
+Author: Stephen Punwasi
+Description: Quickstart template for Wordpress Plugins
+*/
+?>
