bec14ecde2b82a4434d1672fa8b43497910a97b4
Stephen Punwasi <stephen.punwasi@gmail.com>
2014-10-27 12:00:30 -0400

    Added Twitter Button To End of Posts

--- wp-module.php
+++ wp-module.php
@@ -7,11 +7,21 @@ Author: Stephen Punwasi
 Description: Quickstart template for Wordpress Plugins
 */
 
-//ADD ACTION AFTER CALLING THE WP HEAD
-add_action( 'wp_head', 'sp_inject_page_header_output' );
+function pwsocialv1_scripts_function() { ?>
+    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+<?php }
+add_action('wp_footer', 'pwsocialv1_scripts_function');
 
-function sp_inject_page_header_output() { ?>
-//INSERT SCRIPT TO ENTER HERE
- <?php }
+function pwsocialv1_afterposts_function() {
+  $pwsocial_buttons = '<a href="https://twitter.com/share" class="twitter-share-button" data-via="FizWomack">Tweet</a>';
+
+if(is_single() && !is_home()) {
+$content .= $pwsocial_buttons;
+}
+
+return $content;
+}
+
+add_filter('the_content', 'pwsocialv1_afterposts_function');
 
 ?>
